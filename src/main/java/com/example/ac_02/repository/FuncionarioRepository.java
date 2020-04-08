8bba54d72cb70751c07fd519676ce86a3663d83f
Your Name <you@example.com>
2020-04-07 22:49:17 -0300

    apenas teste de commit

--- src/main/java/com/example/ac_02/repository/FuncionarioRepository.java
+++ src/main/java/com/example/ac_02/repository/FuncionarioRepository.java
@@ -3,7 +3,7 @@ package com.example.ac_02.repository;
 import com.example.ac_02.entidade.Funcionario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//banco
 public interface FuncionarioRepository extends JpaRepository<Funcionario,Integer>{
 
 }
